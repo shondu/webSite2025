3c4cf44e4613cb8150ea97f260baa8cb853e7c5c
donmjensen <dmj240@gmail.com>
2024-12-08 14:02:12 -0600

    Create Angular Framework

---
+++ src/app/components/personal-page/personal-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-page',
+  imports: [],
+  templateUrl: './personal-page.component.html',
+  styleUrl: './personal-page.component.css'
+})
+export class PersonalPageComponent {
+
+}
